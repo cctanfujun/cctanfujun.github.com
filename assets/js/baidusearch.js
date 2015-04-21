e00e394500dae3d0c9e1ac90c70c9d75fe5b1b5e
tanfujun <tanfujun@meizu.com>
2015-04-22 03:56:33 +0800

    修改自定义404页面为百度搜索，取消原来google搜索js文件

--- assets/js/baidusearch.js
+++ assets/js/baidusearch.js
@@ -1,13 +1,32 @@
-document.writeln(<form action="http://www.baidu.com/baidu" target="_blank">);
-document.writeln(<table bgcolor="#FFFFFF">);
-document.writeln(<tr>);
-document.writeln(<td>);
-document.writeln(<input name=tn type=hidden value=baidu>);
-document.writeln(<a href="http://www.baidu.com/"><img src="http://img.baidu.com/img/logo-80px.gif" alt="Baidu" align="bottom" border="0">);
-document.writeln(</a>);
-document.writeln(<input type=text name=word size=30>);
-document.writeln(<input type="submit" value="百度搜索">);
-document.writeln(</td>);
-document.writeln(</tr>);
-document.writeln(</table>);
-document.writeln(</form>);
+<script type="text/javascript"> 
+//Code By Dron 
+//Ucren.com 
+document.write((function (){/* 
+
+    <!-- 这里是注释 --> 
+    <table border="1"> 
+        <tr> 
+            <td> 
+                Hello Word! 
+            </td> 
+        </tr> 
+    </table> 
+    <script type="text/javascript"> 
+        alert("Hello Word!"); 
+    <\/script> 
+
+
+
+*/
+<form action="http://www.baidu.com/baidu" target="_blank">
+<table bgcolor="#FFFFFF"><tr><td>
+<input name=tn type=hidden value=baidu>
+<a href="http://www.baidu.com/"><img src="http://img.baidu.com/img/logo-80px.gif" alt="Baidu" align="bottom" border="0"></a>
+<input type=text name=word size=30>
+<input type="submit" value="百度搜索">
+</td></tr></table>
+</form>
+
+}).toString().replace(/^.+?\*|\\(?=\/)|\*.+?$/gi, "")); 
+
+</script>
